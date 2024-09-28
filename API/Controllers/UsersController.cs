2f9cb7c1efb0fada319ac22c97c3ca5facb89364
Jakub Bełtowski <jakbel555@gmail.com>
2024-09-28 22:02:22 +0200

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+  {
+    var users = await context.Users.ToListAsync();
+
+    return users;
+  }
+
+  [HttpGet("{id:int}")] // /api/users/2
+  public async Task<ActionResult<AppUser>> GetUsers(int id)
+  {
+    var user = await context.Users.FindAsync(id);
+
+    if (user == null) return NotFound();
+
+    return user;
+  }
+}
